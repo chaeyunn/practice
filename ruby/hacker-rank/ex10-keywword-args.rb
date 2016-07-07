25a824bd5830acf9ff9a785e32aece5888d88b8c
Hoyoung Jung <honux77@gmail.com>
2016-07-07 11:49:47 +0900

    Hacker rank ruby practice - keyword arguments

---
+++ ruby/hacker-rank/ex10-keywword-args.rb
@@ -0,0 +1,7 @@
+# Your code here
+ def c_to_k(t); t - 273.15; end
+def convert_temp(temp, input_scale: 'fahrenheit', output_scale: 'celsius') 
+    if input_scale == outp def c_to_k(t); t - 273.15; endut_scale
+        return 
+    end
+end
